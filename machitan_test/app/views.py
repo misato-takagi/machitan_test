7463bd3486c51e9d9ef0d4020ea4a0fe6294335f
misato-takagi <3310.takagi.thanks@gmail.com>
2022-12-15 13:40:21 +0900

    カレンダー予約２完了

--- machitan_test/app/views.py
+++ machitan_test/app/views.py
@@ -31,11 +31,14 @@ class CalenderView(View):
     def get(self,request, *args, **kwargs):
         staff_data = Staff.objects.filter(id = self.kwargs['pk']).select_related('user').select_related('store')[0]
         today = date.today()
+        #日付をURLから取得している
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         day = self.kwargs.get('day')
+        #URLに日付が設定されていたら
         if year and month and day:
             start_date = date(year=year,month=month,day=day)
+        #URLに日付が指定されていないなら今日の日付をtoday関数で求める
         else:
             start_date = today
         days = [start_date + timedelta(days=day) for day in range(7)]
@@ -47,24 +50,24 @@ class CalenderView(View):
             row = {}
             for day in days:
                 row[day] = True
-                calender[hour] = row
-            start_time = make_aware(datetime.combine(start_day, time(hour=10,minute=0,second=0)))
-            end_time = make_aware(datetime.combine(end_day,time(hour=20, minute=15,second=0)))
-            booking_data = Booking.objects.filter(staff= staff_data).exclude(Q(start__gt = end_time)| Q(end__lt = start_time ))
-            for booking in booking_data:
-                local_time = localtime(booking.start)
-                booking_date = local_time.date()
-                booking_hour = local_time.hour()
-                if (booking_hour in calender) and (booking_date in calender[booking_hour]):
-                    calender[booking_hour][booking_date] = False
+            calender[hour] = row
+        start_time = make_aware(datetime.combine(start_day, time(hour=10,minute=0,second=0)))
+        end_time = make_aware(datetime.combine(end_day,time(hour=20, minute=15,second=0)))
+        booking_data = Booking.objects.filter(staff= staff_data).exclude(Q(start__gt = end_time)| Q(end__lt = start_time ))
+        for booking in booking_data:
+            local_time = localtime(booking.start)
+            booking_date = local_time.date()
+            booking_hour = local_time.hour()
+            if (booking_hour in calender) and (booking_date in calender[booking_hour]):
+                calender[booking_hour][booking_date] = False
 
-            return render(request, 'app/calender.html',{
-                'staff_data':staff_data,
-                'calender':calender,
-                'days': days,
-                'start_day': start_day,
-                'end_day': end_day,
-                'before':days[0]- timedelta(days=7),
-                'next': days[-1] + timedelta(days=1),
-                'today' : today, 
-            })
+        return render(request, 'app/calender.html',{
+            'staff_data':staff_data,
+            'calender':calender,
+            'days': days,
+            'start_day': start_day,
+            'end_day': end_day,
+            'before':days[0]- timedelta(days=7),
+            'next': days[-1] + timedelta(days=1),
+            'today' : today, 
+        })
